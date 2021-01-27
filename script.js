2238f590eb82b8d12055ad4852728fb22ae3d018
Will <wstechjunior@gmail.com>
2021-01-27 18:09:21 -0300

    Finalized layout and add a modal

---
+++ script.js
@@ -0,0 +1,9 @@
+const modal = {
+    open(){
+        document.querySelector('.modal-overlay').classList.add('active')
+    },
+    close(){
+        document.querySelector('.modal-overlay').classList.remove('active')
+    }
+}
+
